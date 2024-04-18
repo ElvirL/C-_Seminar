79a51029d30e0f505fb8c22f47ca5677c061f7f0
elvir <hh.@mail.ru>
2024-04-18 23:41:21 +0500

    Task3_Ready

---
+++ Lesson1/task3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите трехзначное число: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+int last_digit = num % 10;
+int first_digit = num / 100;
+
+Console.Write(last_digit + first_digit);
